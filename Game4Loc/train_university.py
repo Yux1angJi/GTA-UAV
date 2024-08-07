0f63d492ecd9fb0e86b78c70847ee466869b1127
Yux1angJi <843176830@qq.com>
2024-08-07 05:39:26 -0400

    update train scripts

--- Game4Loc/train_university.py
+++ Game4Loc/train_university.py
@@ -4,6 +4,8 @@ import math
 import shutil
 import sys
 import torch
+import argparse
+
 from dataclasses import dataclass
 from torch.cuda.amp import GradScaler
 from torch.utils.data import DataLoader
@@ -16,30 +18,53 @@ from game4loc.evaluate.university import evaluate
 from game4loc.loss import InfoNCE
 from game4loc.model import TimmModel
 
+def parse_tuple(s):
+    try:
+        return tuple(map(int, s.split(',')))
+    except ValueError:
+        raise argparse.ArgumentTypeError("Tuple must be integers separated by commas")
 
 @dataclass
 class Configuration:
     
     # Model
-    # model: str = 'convnext_base.fb_in22k_ft_in1k_384'
     model: str = 'vit_base_patch16_rope_reg1_gap_256.sbb_in1k'
     
     # Override model image size
     img_size: int = 384
+ 
+    freeze_layers: bool = False
 
-    num_chunks: int = 1
+    frozen_stages = [0,0,0,0]
+
+    share_weights: bool = True
+    
+    with_weight: bool = True
+
+    train_in_group: bool = True
+    group_len = 2
+
+    loss_type = ["whole_slice", "part_slice"]
+
+    train_with_mix_data: bool = False
+
+    train_with_recon: bool = False
+    recon_weight: float = 0.1
+    
     
     # Training 
     mixed_precision: bool = True
     custom_sampling: bool = True         # use custom sampling instead of random
     seed = 1
-    epochs: int = 1
-    batch_size: int = 64                # keep in mind real_batch_size = 2 * batch_size
+    epochs: int = 10
+    batch_size: int = 40                # keep in mind real_batch_size = 2 * batch_size
     verbose: bool = False
-    gpu_ids: tuple = 0           # GPU ids for training
-    
+    gpu_ids: tuple = (0,1)           # GPU ids for training
+
+    train_ratio: float = 1.0
+
     # Eval
-    batch_size_eval: int = 64
+    batch_size_eval: int = 128
     eval_every_n_epoch: int = 1          # eval every n Epoch
     normalize_features: bool = True
     eval_gallery_n: int = -1             # -1 for all or int
@@ -51,17 +76,14 @@ class Configuration:
     
     # Loss
     label_smoothing: float = 0.1
+    k: float = 3
     
     # Learning Rate
     lr: float = 0.001                    # 1 * 10^-4 for ViT | 1 * 10^-1 for CNN
     scheduler: str = "cosine"           # "polynomial" | "cosine" | "constant" | None
     warmup_epochs: int = 0.1
     lr_end: float = 0.0001               #  only for "polynomial"
-    
-    # Dataset
-    dataset: str = 'U1652-D2S'           # 'U1652-D2S' | 'U1652-S2D'
-    data_folder: str = "./data/U1652"
-    
+
     # Augment Images
     prob_flip: float = 0.5              # flipping the sat image and drone image simultaneously
     
@@ -73,7 +95,8 @@ class Configuration:
     
     # Checkpoint to start from
     checkpoint_start = None
-  
+    # checkpoint_start = "/home/xmuairmud/jyx/ExtenGeo/Game4Loc/pretrained/university/convnext_base.fb_in22k_ft_in1k_384/weights_e1_0.9515.pth"
+
     # set num_workers to 0 if on Windows
     num_workers: int = 0 if os.name == 'nt' else 4 
     
@@ -86,26 +109,34 @@ class Configuration:
     # make cudnn deterministic
     cudnn_deterministic: bool = False
 
+    query_folder_train = '/home/xmuairmud/data/University-Release/train/satellite'
+    gallery_folder_train = '/home/xmuairmud/data/University-Release/train/drone'   
+    query_folder_test = '/home/xmuairmud/data/University-Release/test/query_drone' 
+    gallery_folder_test = '/home/xmuairmud/data/University-Release/test/gallery_satellite'  
+
 
 #-----------------------------------------------------------------------------#
 # Train Config                                                                #
 #-----------------------------------------------------------------------------#
 
-config = Configuration() 
+# config = Configuration() 
 
-if config.dataset == 'U1652-D2S':
-    config.query_folder_train = '/home/xmuairmud/data/University-Release/train/satellite'
-    config.gallery_folder_train = '/home/xmuairmud/data/University-Release/train/drone'   
-    config.query_folder_test = '/home/xmuairmud/data/University-Release/test/query_drone' 
-    config.gallery_folder_test = '/home/xmuairmud/data/University-Release/test/gallery_satellite'  
-elif config.dataset == 'U1652-S2D':
-    config.query_folder_train = './data/U1652/train/satellite'
-    config.gallery_folder_train = './data/U1652/train/drone'    
-    config.query_folder_test = './data/U1652/test/query_satellite'
-    config.gallery_folder_test = './data/U1652/test/gallery_drone'
+# # if config.dataset == 'U1652-D2S':
+# config.query_folder_train = '/home/xmuairmud/data/University-Release/train/satellite'
+# config.gallery_folder_train = '/home/xmuairmud/data/University-Release/train/drone'   
+# config.query_folder_test = '/home/xmuairmud/data/University-Release/test/query_drone' 
+# config.gallery_folder_test = '/home/xmuairmud/data/University-Release/test/gallery_satellite'  
+# elif config.dataset == 'U1652-S2D':
+#     config.query_folder_train = './data/U1652/train/satellite'
+#     config.gallery_folder_train = './data/U1652/train/drone'    
+#     config.query_folder_test = './data/U1652/test/query_satellite'
+#     config.gallery_folder_test = './data/U1652/test/gallery_drone'
 
 
-if __name__ == '__main__':
+def train_script(config):
+    f = open(config.log_path, 'w')
+    if config.log_to_file:
+        sys.stdout = f
 
     model_path = "{}/{}/{}".format(config.model_path,
                                        config.model,
@@ -322,7 +353,7 @@ if __name__ == '__main__':
                            optimizer=optimizer,
                            scheduler=scheduler,
                            scaler=scaler,
-                           num_chunks=config.num_chunks)
+                           )
         
         print("Epoch: {}, Train Loss = {:.3f}, Lr = {:.6f}".format(epoch,
                                                                    train_loss,
@@ -357,4 +388,94 @@ if __name__ == '__main__':
     if torch.cuda.device_count() > 1 and len(config.gpu_ids) > 1:
         torch.save(model.module.state_dict(), '{}/weights_end.pth'.format(model_path))
     else:
-        torch.save(model.state_dict(), '{}/weights_end.pth'.format(model_path))            
+        torch.save(model.state_dict(), '{}/weights_end.pth'.format(model_path))      
+
+    f.close()
+    sys.stdout = sys.__stdout__   
+
+      
+def parse_args():
+    parser = argparse.ArgumentParser(description="Training script for gta.")
+
+    parser.add_argument('--log_to_file', action='store_true', help='Log saving to file')
+
+    parser.add_argument('--log_path', type=str, default=None, help='Log file path')
+
+    parser.add_argument('--data_dir', type=str, default='randcam2_std0_stable/test', help='Data path')
+
+    parser.add_argument('--model', type=str, default='convnext_base.fb_in22k_ft_in1k_384', help='Model architecture')
+
+    parser.add_argument('--no_share_weights', action='store_true', help='Train without sharing wieghts')
+
+    parser.add_argument('--freeze_layers', action='store_true', help='Freeze layers for training')
+
+    parser.add_argument('--frozen_stages', type=int, nargs='+', default=[0,0,0,0], help='Frozen stages for training')
+
+    parser.add_argument('--epochs', type=int, default=5, help='Epochs')
+
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+
+    parser.add_argument('--gpu_ids', type=parse_tuple, default=(0,1), help='GPU ID')
+
+    parser.add_argument('--batch_size', type=int, default=40, help='Batch size')
+
+    parser.add_argument('--checkpoint_start', type=str, default=None, help='Training from checkpoint')
+
+    parser.add_argument('--train_mode', type=str, default='iou', help='Train with pair in iou or oc')
+
+    parser.add_argument('--test_mode', type=str, default='oc', help='Test with pair in iou or oc')
+
+    parser.add_argument('--train_with_recon', action='store_true', help='Train with reconstruction')
+
+    parser.add_argument('--recon_weight', type=float, default=0.1, help='Loss weight for reconstruction')
+
+    parser.add_argument('--train_in_group', action='store_true', help='Train in group')
+    
+    parser.add_argument('--group_len', type=int, default=2, help='Group length')
+
+    parser.add_argument('--train_with_mix_data', action='store_true', help='Train with mix data')
+
+    parser.add_argument('--loss_type', type=str, nargs='+', default=['part_slice', 'whole_slice'], help='Loss type for group train')
+
+    parser.add_argument('--label_smoothing', type=float, default=0.0, help='Label smoothing value for loss')
+
+    parser.add_argument('--with_weight', action='store_true', help='Train with weight')
+
+    parser.add_argument('--k', type=float, default=5, help='weighted k')
+    
+    parser.add_argument('--train_ratio', type=float, default=1.0, help='Train on ratio of data')
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    config = Configuration()
+    config.data_dir = args.data_dir
+    config.log_to_file = args.log_to_file
+    config.log_path = args.log_path
+    config.epochs = args.epochs
+    config.batch_size = args.batch_size
+    config.train_in_group = args.train_in_group
+    config.train_with_recon = args.train_with_recon
+    config.recon_weight = args.recon_weight
+    config.group_len = args.group_len
+    config.train_with_mix_data = args.train_with_mix_data
+    config.loss_type = args.loss_type
+    config.gpu_ids = args.gpu_ids
+    config.label_smoothing = args.label_smoothing
+    config.with_weight = args.with_weight
+    config.k = args.k
+    config.checkpoint_start = args.checkpoint_start
+    config.model = args.model
+    config.lr = args.lr
+    config.share_weights = not(args.no_share_weights)
+    config.freeze_layers = args.freeze_layers
+    config.frozen_stages = args.frozen_stages
+    config.train_mode = args.train_mode
+    config.test_mode = args.test_mode
+    config.train_ratio = args.train_ratio
+
+    train_script(config)
