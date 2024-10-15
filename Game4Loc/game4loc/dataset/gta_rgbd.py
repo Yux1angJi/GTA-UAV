add rgbd training and fix rotation bug

Game4Loc/game4loc/dataset/gta_rgbd.py
@@ -16,6 +16,7 @@ import concurrent.futures
 import itertools
 import pickle
 import json
+import torch
 
 
 SATE_LENGTH = 24576
@@ -39,7 +40,7 @@ def get_sate_data(root_dir):
     return sate_img_dir_list, sate_img_list
 
 
-class GTADatasetTrain(Dataset):
+class GTARGBDDatasetTrain(Dataset):
     
     def __init__(self,
                  pairs_meta_file,
@@ -65,6 +66,7 @@ class GTADatasetTrain(Dataset):
 
         for pair_drone2sate in pairs_meta_data:
             drone_img_dir = pair_drone2sate['drone_img_dir']
+            drone_img_dir = drone_img_dir.replace('images', 'rgbd')
             drone_img_name = pair_drone2sate['drone_img_name']
             sate_img_dir = pair_drone2sate['sate_img_dir']
             # Training with Positive-only data or Positive+Semi-positive data
@@ -102,19 +104,24 @@ class GTADatasetTrain(Dataset):
         query_img_path, gallery_img_path, positive_weight = self.samples[index]
         
         # for query there is only one file in folder
-        query_img = cv2.imread(query_img_path)
-        query_img = cv2.cvtColor(query_img, cv2.COLOR_BGR2RGB)
+        query_img = cv2.imread(query_img_path, cv2.IMREAD_UNCHANGED)
         
         gallery_img = cv2.imread(gallery_img_path)
         gallery_img = cv2.cvtColor(gallery_img, cv2.COLOR_BGR2RGB)
         
         if np.random.random() < self.prob_flip:
             query_img = cv2.flip(query_img, 1)
-            gallery_img = cv2.flip(gallery_img, 1) 
         
         # image transforms
         if self.transforms_query is not None:
-            query_img = self.transforms_query(image=query_img)['image']
+            image_rgb = query_img[:, :, :3]
+            image_d = query_img[:, :, 3]
+            transformed = self.transforms_query(image=image_rgb, image_d=image_d)
+            image_rgb_transformed = transformed['image']
+            image_d_transformed = transformed['image_d']
+            if image_d_transformed.ndim == 2:
+                image_d_transformed = image_d_transformed.unsqueeze(0)
+            query_img = torch.cat((image_rgb_transformed, image_d_transformed), dim=0)  # 形状为 (4, H, W)
             
         if self.transforms_gallery is not None:
             gallery_img = self.transforms_gallery(image=gallery_img)['image']
@@ -123,142 +130,6 @@ class GTADatasetTrain(Dataset):
 
     def __len__(self):
         return len(self.samples)
-
-    def shuffle_group(self, ):
-        '''
-        Implementation of Mutually Exclusive Sampling process with group
-        '''
-        print("\nShuffle Dataset in Batches:")
-        
-        pair_pool = copy.deepcopy(self.pairs)
-        # Shuffle pairs order
-        random.shuffle(pair_pool)
-        
-        sate_batch = set()
-        drone_batch = set()
-        
-        # Lookup if already used in epoch
-        pairs_epoch = set()   
-
-        # buckets
-        batches = []
-        current_batch = []
-        
-        # counter
-        break_counter = 0
-
-        while True:
-            # pbar.update()
-            # print(break_counter)
-            if len(pair_pool) > 0:
-                if break_counter >= 16384:
-                    break
-
-                pair = pair_pool.pop(0)
-                
-                drone_img_path, sate_img_path, _ = pair
-                drone_img_dir = os.path.dirname(drone_img_path)
-                sate_img_dir = os.path.dirname(sate_img_path)
-
-                drone_img_name_i = drone_img_path.split('/')[-1]
-                sate_img_name_i = sate_img_path.split('/')[-1]
-
-                pair_name = (drone_img_name_i, sate_img_name_i)
-
-                if drone_img_name_i in drone_batch or pair_name in pairs_epoch:
-                    if pair_name not in pairs_epoch:
-                            pair_pool.append(pair)
-                    break_counter += 1
-                    continue
-
-                pairs_drone2sate = self.pairs_drone2sate_dict[drone_img_name_i]
-                random.shuffle(pairs_drone2sate)
-
-                subset_sate_len = itertools.combinations(pairs_drone2sate, self.group_len)
-                
-                subset_drone = None
-                subset_sate = None
-                for subset_sate_i in subset_sate_len:
-                    flag = True
-                    sate2drone_inter_set = None
-
-                    #### Check for sate
-                    for sate_img in subset_sate_i:
-                        if sate_img in sate_batch:
-                            flag = False
-                            break
-                        
-                        if sate2drone_inter_set == None:
-                            sate2drone_inter_set = set(self.pairs_sate2drone_dict[sate_img])
-                        else:
-                            sate2drone_inter_set = sate2drone_inter_set.intersection(self.pairs_sate2drone_dict[sate_img])
-                    
-                        
-                    if not flag or sate2drone_inter_set == None or len(sate2drone_inter_set) < self.group_len:
-                        continue
-
-                    sate2drone_inter_set = list(sate2drone_inter_set)
-                    random.shuffle(sate2drone_inter_set)
-                    subset_drone_len = itertools.combinations(sate2drone_inter_set, self.group_len)
-                    #### Check for drone
-                    for subset_drone_i in subset_drone_len:
-                        if drone_img_name_i not in subset_drone_i:
-                            continue
-                        flag = True
-                        for drone_img in subset_drone_i:
-                            if drone_img in drone_batch or flag == False:
-                                flag = False
-                                break
-                            for sate_img in subset_sate_i:
-                                pair_tmp = (drone_img, sate_img)
-                                if pair_tmp in pairs_epoch:
-                                    flag = False
-                                    break
-                        if flag:
-                            subset_drone = subset_drone_i
-                            subset_sate = subset_sate_i
-                            break
-                
-                if subset_drone != None and subset_sate != None:
-                    # random.shuffle(subset_drone)
-                    # random.shuffle(subset_sate)
-                    for drone_img_name, sate_img_name in zip(subset_drone, subset_sate):
-                        drone_img_path = os.path.join(self.data_root, drone_img_dir, drone_img_name)
-                        sate_img_path = os.path.join(self.data_root, sate_img_dir, sate_img_name)
-                        current_batch.append((drone_img_path, sate_img_path, 1.0))
-                        pairs_epoch.add((drone_img_name, sate_img_name))
-                    for drone_img in subset_drone:
-                        pairs_drone2sate = self.pairs_drone2sate_dict[drone_img_name]
-                        for sate in pairs_drone2sate:
-                            sate_batch.add(sate)
-                    for sate_img in subset_sate:
-                        pairs_sate2drone = self.pairs_sate2drone_dict[sate_img_name]
-                        for drone in pairs_sate2drone:
-                            drone_batch.add(drone)
-                else:
-                    if pair_name not in pairs_epoch:
-                            pair_pool.append(pair)        
-                    break_counter += 1
-
-                if break_counter >= 16384:
-                    break
-            else:
-                break
-            if len(current_batch) >= self.shuffle_batch_size:
-                # empty current_batch bucket to batches
-                batches.extend(current_batch)
-                sate_batch = set()
-                drone_batch = set()
-                current_batch = []
-        
-        self.samples = batches
-        
-        print("Original Length: {} - Length after Shuffle: {}".format(len(self.pairs), len(self.samples))) 
-        print("Break Counter:", break_counter)
-        print("Pairs left out of last batch to avoid creating noise:", len(self.pairs) - len(self.samples))
-        print("First Element: {} - Last Element: {}".format(self.samples[0][0], self.samples[-1][0]))  
-        print("First Element: {} - Last Element: {}".format(self.samples[0][1], self.samples[-1][1]))  
-    
 
     def shuffle(self, ):
         '''
@@ -349,7 +220,7 @@ class GTADatasetTrain(Dataset):
         print("First Element: {} - Last Element: {}".format(self.samples[0][1], self.samples[-1][1]))  
         
 
-class GTADatasetEval(Dataset):
+class GTARGBDDatasetEval(Dataset):
     
     def __init__(self,
                  pairs_meta_file,
@@ -376,10 +247,13 @@ class GTADatasetEval(Dataset):
         self.pairs_drone2sate_dict = {}
         self.pairs_match_set = set()
 
+        self.view = view
+
         if view == 'drone':
             for pair_drone2sate in pairs_meta_data:
                 drone_img_name = pair_drone2sate['drone_img_name']
                 drone_img_dir = pair_drone2sate['drone_img_dir']
+                drone_img_dir = drone_img_dir.replace('images', 'rgbd')
                 drone_loc_x_y = pair_drone2sate['drone_loc_x_y']
                 self.pairs_drone2sate_dict[drone_img_name] = []
                 pair_sate_img_list = pair_drone2sate[f'pair_{mode}_sate_img_list']
@@ -428,11 +302,21 @@ class GTADatasetEval(Dataset):
         
         img_path = self.images_path[index]
         
-        img = cv2.imread(img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        
-        # image transforms
-        if self.transforms is not None:
+        if self.view == 'drone':
+            img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+            image_rgb = img[:, :, :3]
+            image_d = img[:, :, 3]
+            transformed = self.transforms(image=image_rgb, image_d=image_d)
+            image_rgb_transformed = transformed['image']
+            image_d_transformed = transformed['image_d']
+            if image_d_transformed.ndim == 2:
+                image_d_transformed = image_d_transformed.unsqueeze(0)
+            img = torch.cat((image_rgb_transformed, image_d_transformed), dim=0)  # 形状为 (4, H, W)
+
+
+        else:
+            img = cv2.imread(img_path)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img = self.transforms(image=img)['image']
         
         return img
@@ -444,12 +328,19 @@ class GTADatasetEval(Dataset):
 def get_transforms(img_size,
                    mean=[0.485, 0.456, 0.406],
                    std=[0.229, 0.224, 0.225]):
+
+    mean_rgbd = mean + [0.5]
+    std_rgbd = std + [0.5]
     
 
-    val_transforms = A.Compose([A.Resize(img_size[0], img_size[1], interpolation=cv2.INTER_LINEAR_EXACT, p=1.0),
+    val_sat_transforms = A.Compose([A.Resize(img_size[0], img_size[1], interpolation=cv2.INTER_LINEAR_EXACT, p=1.0),
                                 A.Normalize(mean, std),
                                 ToTensorV2(),
                                 ])
+    val_drone_transforms = A.Compose([A.Resize(img_size[0], img_size[1], interpolation=cv2.INTER_LINEAR_EXACT, p=1.0),
+                                A.Normalize(mean_rgbd, std_rgbd),
+                                ToTensorV2(),
+                                ],additional_targets={'image_d': 'image'})
                                 
 
     train_sat_transforms = A.Compose([A.ImageCompression(quality_lower=90, quality_upper=100, p=0.5),
@@ -469,7 +360,6 @@ def get_transforms(img_size,
                                                                min_width=int(0.1*img_size[0]),
                                                                p=1.0),
                                               ], p=0.3),
-                                      # A.RandomRotate90(p=1.0),
                                       A.Normalize(mean, std),
                                       ToTensorV2(),
                                       ])
@@ -492,11 +382,12 @@ def get_transforms(img_size,
                                                                  p=1.0),
                                               ], p=0.3),
                                         A.RandomRotate90(p=1.0),
-                                        A.Normalize(mean, std),
+                                        A.Normalize(mean_rgbd, std_rgbd),
                                         ToTensorV2(),
-                                        ])
+                                        ],
+                                        additional_targets={'image_d': 'image'})
     
-    return val_transforms, train_sat_transforms, train_drone_transforms
+    return val_sat_transforms, val_drone_transforms, train_sat_transforms, train_drone_transforms
 
 
 if __name__ == "__main__":

Game4Loc/game4loc/dataset/pc_utils.py
@@ -1,6 +1,88 @@
-### Copy from Uni3D
+### Copy from Uni3D and lidar-image-pretrain-VPR
 
 import numpy as np
+from PIL import Image
+
+
+def create_top_down_depth_image(points, bev_height=1080, bev_width=1920):
+    """
+    将点云数据生成俯视角深度图像（鸟瞰图），自动计算投影范围。
+
+    Args:
+        points: 点云数据，形状为 (N, 3)，每个点包含 (x, y, z)
+        bev_height: 输出图像的高度（像素）
+        bev_width: 输出图像的宽度（像素）
+
+    Returns:
+        depth_map_uint8: 归一化后的深度图像，dtype 为 uint8
+    """
+    # 提取点云坐标
+    x_points = points[:, 0]
+    y_points = points[:, 1]
+    z_points = points[:, 2]
+
+    # 自动计算投影范围
+    x_min = np.min(x_points)
+    x_max = np.max(x_points)
+    y_min = np.min(y_points)
+    y_max = np.max(y_points)
+
+    # 如果需要，可以添加边距
+    padding = 0  # 根据需要调整边距大小
+    x_min -= padding
+    x_max += padding
+    y_min -= padding
+    y_max += padding
+
+    # 计算范围和分辨率
+    x_range = x_max - x_min
+    y_range = y_max - y_min
+    x_resolution = x_range / (bev_width - 1)    # 修改此处
+    y_resolution = y_range / (bev_height - 1)   # 修改此处
+
+    # 将物理坐标转换为图像坐标
+    x_img = ((x_points - x_min) / x_resolution).astype(np.int32)
+    y_img = ((y_points - y_min) / y_resolution).astype(np.int32)
+
+    # 确保索引在合法范围内
+    x_img = np.clip(x_img, 0, bev_width - 1)
+    y_img = np.clip(y_img, 0, bev_height - 1)
+
+    # 将图像坐标系原点设置在左下角
+    y_img = bev_height - y_img - 1
+
+    # 初始化深度图
+    depth_map = np.zeros((bev_height, bev_width), dtype=np.float32)
+
+    # 处理遮挡关系，保留最高点（z 值最大的点）
+    indices = np.lexsort((-z_points, y_img, x_img))
+    x_img = x_img[indices]
+    y_img = y_img[indices]
+    z_points = z_points[indices]
+    coords = np.stack((y_img, x_img), axis=1)
+    _, unique_indices = np.unique(coords, axis=0, return_index=True)
+    depth_map[y_img[unique_indices], x_img[unique_indices]] = z_points[unique_indices]
+    print(depth_map[0][0])
+    # 将深度值归一化到 [0, 255]
+    non_zero_depth = depth_map[depth_map != 0]
+    if len(non_zero_depth) > 0:
+        max_height = np.percentile(non_zero_depth, 99)
+        min_height = np.percentile(non_zero_depth, 1)
+        depth_map_clipped = np.clip(depth_map, min_height, max_height)
+        print(depth_map_clipped[0][0])
+        depth_map_normalized = (depth_map_clipped - min_height) / (max_height - min_height)
+        print(depth_map_normalized[0][0])
+        print(min_height, max_height)
+        depth_map_uint8 = (depth_map_normalized * 255).astype(np.uint8)
+    else:
+        depth_map_uint8 = depth_map.astype(np.uint8)
+    
+    
+    colored_depth_map = cv2.applyColorMap(depth_map_uint8, cv2.COLORMAP_JET)
+
+    return colored_depth_map 
+
+
 
 def random_rotate_z(pc):
     # random roate around z axis
@@ -128,3 +210,19 @@ def augment_pc(data):
     data = rotate_perturbation_point_cloud(data)
     data = data.squeeze()
     return data
+
+
+if __name__ == '__main__':
+    import open3d as o3d
+    import cv2
+    points = o3d.io.read_point_cloud('/home/xmuairmud/data/GTA-UAV-data/Lidar/drone/lidars/200_0001_0000006317.ply')
+    points = np.array(points.points, dtype=np.float32)
+
+    points[:, [0, 2]] = points[:, [2, 0]]  # 交换第0列(x)和第2列(z)
+    points[:, [0, 1]] = points[:, [1, 0]]  # 交换第0列(x)和第2列(z)
+    points[:, 0] = -points[:, 0]
+    points[:, 1] = points[:, 1]
+    points[:, 2] = 10000-points[:, 2]
+
+    image = create_top_down_depth_image(points)
+    cv2.imwrite('test_lidar_range.png', image)

Game4Loc/game4loc/loss.py
@@ -275,11 +275,20 @@ class TripletLoss(nn.Module):
 
 
 class MMWeightedInfoNCE(nn.Module):
-    def __init__(self, label_smoothing, k=5, device='cuda' if torch.cuda.is_available() else 'cpu'):
+    def __init__(self, 
+                 label_smoothing=0.05, 
+                 k=5,
+                 dimg2simg=True,
+                 dlidar2simg=True,
+                 dimg2dlidar=True,
+                 device='cuda' if torch.cuda.is_available() else 'cpu'):
         super().__init__()
         self.label_smoothing = label_smoothing
         self.device = device
         self.k = k
+        self.dimg2simg = dimg2simg
+        self.dlidar2simg = dlidar2simg
+        self.dimg2dlidar = dimg2dlidar
 
     def loss(self, similarity_matrix, eps_all):
         n = similarity_matrix.shape[0]
@@ -321,9 +330,18 @@ class MMWeightedInfoNCE(nn.Module):
         logits_drone_lidar2satellite_img = logit_scale * drone_lidar_features @ satellite_img_features.T
         logits_satellite_img2drone_lidar = logits_drone_lidar2satellite_img.T
 
-        loss_drone_img_drone_lidar = (self.loss(logits_drone_img2drone_lidar, eps) + self.loss(logits_drone_lidar2drone_img, eps)) / 2
-        loss_drone_img_satellite_img = (self.loss(logits_drone_img2satellite_img, eps) + self.loss(logits_satellite_img2drone_img, eps)) / 2
-        loss_drone_lidar_satellite_img = (self.loss(logits_drone_lidar2satellite_img, eps) + self.loss(logits_satellite_img2drone_lidar, eps)) / 2
+        if self.dimg2dlidar:
+            loss_drone_img_drone_lidar = (self.loss(logits_drone_img2drone_lidar, eps) + self.loss(logits_drone_lidar2drone_img, eps)) / 2
+        else:
+            loss_drone_img_drone_lidar = 0.
+        if self.dimg2simg:
+            loss_drone_img_satellite_img = (self.loss(logits_drone_img2satellite_img, eps) + self.loss(logits_satellite_img2drone_img, eps)) / 2
+        else:
+            loss_drone_img_satellite_img = 0.
+        if self.dlidar2simg:
+            loss_drone_lidar_satellite_img = (self.loss(logits_drone_lidar2satellite_img, eps) + self.loss(logits_satellite_img2drone_lidar, eps)) / 2
+        else:
+            loss_drone_lidar_satellite_img = 0.
 
         return {
             "contrastive_drone_img_drone_lidar": loss_drone_img_drone_lidar,

Game4Loc/game4loc/trainer_mm.py
@@ -88,47 +88,8 @@ def train_mm_with_weight(train_config, model, dataloader, loss_function, optimiz
             if train_config.scheduler == "polynomial" or train_config.scheduler == "cosine" or train_config.scheduler ==  "constant":
                 scheduler.step()
     
-        # else:
-        
-        #     query = query.to(train_config.device)
-            # reference = reference.to(train_config.device)
-            # weight = weight.to(train_config.device)
-        
-            # # # Forward pass
-            # loss = {}
-
-            # features1, features2 = model(img1=query, img2=reference)
-
-            # if torch.cuda.device_count() > 1 and len(train_config.gpu_ids) > 1: 
-            #     if with_weight:
-            #         loss_sim = loss_function(features1, features2, model.module.logit_scale.exp(), weight)
-            #     else:
-            #         loss_sim = loss_function(features1, features2, model.module.logit_scale.exp())
-            # else:
-            #     if with_weight:
-            #         loss_sim = loss_function(features1, features2, model.logit_scale.exp(), weight)
-            #     else: 
-            #         loss_sim = loss_function(features1, features2, model.logit_scale.exp()) 
-            
-            # loss.update(loss_sim)
-            
-            # loss_total = sum(loss.values())
-            # losses.update(loss_total.item())
-            
-            # loss_total.backward()
-            
-            # # Gradient clipping 
-            # if train_config.clip_grad:
-            #     torch.nn.utils.clip_grad_value_(model.parameters(), train_config.clip_grad)                  
-            
-            # # Update model parameters (weights)
-            # optimizer.step()
-            # # Zero gradients for next step
-            # optimizer.zero_grad()
-            
-            # # Scheduler
-            # if train_config.scheduler == "polynomial" or train_config.scheduler == "cosine" or train_config.scheduler ==  "constant":
-            #     scheduler.step()
+        else:
+            raise NotImplementedError
         
         if train_config.verbose:
             

Game4Loc/train_gta_rgbd.py
@@ -10,12 +10,12 @@ from torch.cuda.amp import GradScaler
 from torch.utils.data import DataLoader
 from transformers import get_constant_schedule_with_warmup, get_polynomial_decay_schedule_with_warmup, get_cosine_schedule_with_warmup
 
-from game4loc.dataset.gta_mm import GTAMMDatasetEval, GTAMMDatasetTrain, get_transforms
+from game4loc.dataset.gta_rgbd import GTARGBDDatasetEval, GTARGBDDatasetTrain, get_transforms
 from game4loc.utils import setup_system, Logger
-from game4loc.trainer_mm import train_mm_with_weight
-from game4loc.evaluate.gta_mm import evaluate
-from game4loc.loss import InfoNCE, MMWeightedInfoNCE, WeightedInfoNCE, GroupInfoNCE, TripletLoss
-from game4loc.models.model_lidar import DesModelWithPC
+from game4loc.trainer import train, train_with_weight
+from game4loc.evaluate.gta import evaluate
+from game4loc.loss import InfoNCE, WeightedInfoNCE, GroupInfoNCE, TripletLoss
+from game4loc.models.model_rgbd import DesModelWithRGBD
 
 
 def parse_tuple(s):
@@ -162,15 +162,15 @@ def train_script(config):
     #-----------------------------------------------------------------------------#
     print("\nModel: {}".format(config.model))
 
-    model = DesModelWithPC(model_name=config.model, 
+    model = DesModelWithRGBD(model_name=config.model, 
                     pretrained=True,
                     img_size=config.img_size,
                     share_weights=config.share_weights)
                         
     data_config = model.get_config()
     print(data_config)
-    mean = data_config["mean"]
-    std = data_config["std"]
+    mean = list(data_config["mean"])
+    std = list(data_config["std"])
     img_size = (config.img_size, config.img_size)
     
     # Activate gradient checkpointing
@@ -207,14 +207,15 @@ def train_script(config):
     # DataLoader                                                                  #
     #-----------------------------------------------------------------------------#
 
+    print('jyxjyxjyx input', mean, std)
     # Transforms
-    val_transforms, train_sat_transforms, train_drone_transforms = get_transforms(img_size, mean=mean, std=std)
-                                                                                                              
+    val_sat_transforms, val_drone_transforms, train_sat_transforms, train_drone_transforms = get_transforms(img_size, mean=mean, std=std)
+                                                                                                                                 
     # Train
-    train_dataset = GTAMMDatasetTrain(data_root=config.data_root,
+    train_dataset = GTARGBDDatasetTrain(data_root=config.data_root,
                                     pairs_meta_file=config.train_pairs_meta_file,
-                                    transforms_satellite=train_sat_transforms,
-                                    transforms_drone=train_drone_transforms,
+                                    transforms_query=train_drone_transforms,
+                                    transforms_gallery=train_sat_transforms,
                                     group_len=config.group_len,
                                     prob_flip=config.prob_flip,
                                     shuffle_batch_size=config.batch_size,
@@ -229,22 +230,22 @@ def train_script(config):
                                   pin_memory=True)
     
     # Test query
-    if config.query_mode == 'DImg2SImg':
-        query_view = 'drone_img'
-        gallery_view = 'sate_img'
-    elif config.query_mode == 'DLidar2SImg':
-        query_view = 'drone_lidar'
-        gallery_view = 'sate_img'
-    query_dataset_test = GTAMMDatasetEval(data_root=config.data_root,
+    if config.query_mode == 'D2S':
+        query_view = 'drone'
+        gallery_view = 'sate'
+    else:
+        query_view = 'sate'
+        gallery_view = 'drone'
+    query_dataset_test = GTARGBDDatasetEval(data_root=config.data_root,
                                         pairs_meta_file=config.test_pairs_meta_file,
                                         view=query_view,
-                                        transforms=val_transforms,
+                                        transforms=val_drone_transforms,
                                         mode=config.test_mode,
                                         sate_img_dir=config.sate_img_dir,
                                         query_mode=config.query_mode,
                                         )
-    query_img_list = query_dataset_test.drone_img_names
-    query_loc_xy_list = query_dataset_test.drone_loc_xys
+    query_img_list = query_dataset_test.images_name
+    query_loc_xy_list = query_dataset_test.images_loc_xy
     pairs_drone2sate_dict = query_dataset_test.pairs_drone2sate_dict
     
     query_dataloader_test = DataLoader(query_dataset_test,
@@ -254,16 +255,16 @@ def train_script(config):
                                        pin_memory=True)
     
     # Test gallery
-    gallery_dataset_test = GTAMMDatasetEval(data_root=config.data_root,
+    gallery_dataset_test = GTARGBDDatasetEval(data_root=config.data_root,
                                           pairs_meta_file=config.test_pairs_meta_file,
                                           view=gallery_view,
-                                          transforms=val_transforms,
+                                          transforms=val_sat_transforms,
                                           mode=config.test_mode,
                                           sate_img_dir=config.sate_img_dir,
                                           query_mode=config.query_mode,
                                          )
-    gallery_loc_xy_list = gallery_dataset_test.satellite_loc_xys
-    gallery_img_list = gallery_dataset_test.satellite_img_names
+    gallery_loc_xy_list = gallery_dataset_test.images_loc_xy
+    gallery_img_list = gallery_dataset_test.images_name
     
     gallery_dataloader_test = DataLoader(gallery_dataset_test,
                                        batch_size=config.batch_size_eval,
@@ -279,13 +280,10 @@ def train_script(config):
     #-----------------------------------------------------------------------------#
     print("Train with weight?", config.with_weight, "k=", config.k)
     
-    loss_function_normal = MMWeightedInfoNCE(
+    loss_function_normal = WeightedInfoNCE(
         device=config.device,
         label_smoothing=config.label_smoothing,
         k=config.k,
-        dimg2simg=False,
-        dlidar2simg=False,
-        dimg2dlidar=True,
     )
     ## For TripletLoss
     # loss_function_normal = TripletLoss(device=config.device)
@@ -353,12 +351,6 @@ def train_script(config):
     #-----------------------------------------------------------------------------#
     # Zero Shot                                                                   #
     #-----------------------------------------------------------------------------#
-    if config.query_mode == 'DImg2SImg':
-        query_feature = 'drone_img_features'
-        gallery_feature = 'satellite_img_features'
-    elif config.query_mode == 'DLidar2SImg':
-        query_feature = 'drone_lidar_features'
-        gallery_feature = 'satellite_img_features'
     if config.zero_shot:
         print("\n{}[{}]{}".format(30*"-", "Zero Shot", 30*"-"))  
 
@@ -367,9 +359,7 @@ def train_script(config):
                            query_loader=query_dataloader_test,
                            gallery_loader=gallery_dataloader_test, 
                            query_list=query_img_list,
-                           query_feature=query_feature,
                            gallery_list=gallery_img_list,
-                           gallery_feature=gallery_feature,
                            pairs_dict=pairs_drone2sate_dict,
                            ranks_list=[1, 5, 10],
                            query_loc_xy_list=query_loc_xy_list,
@@ -392,7 +382,7 @@ def train_script(config):
         if config.custom_sampling:
             train_dataloader.dataset.shuffle()
         
-        train_loss = train_mm_with_weight(config,
+        train_loss = train_with_weight(config,
                            model,
                            dataloader=train_dataloader,
                            loss_function=loss_function_normal,
@@ -416,8 +406,6 @@ def train_script(config):
                                 gallery_loader=gallery_dataloader_test, 
                                 query_list=query_img_list,
                                 gallery_list=gallery_img_list,
-                                query_feature=query_feature,
-                                gallery_feature=gallery_feature,
                                 pairs_dict=pairs_drone2sate_dict,
                                 ranks_list=[1, 5, 10],
                                 query_loc_xy_list=query_loc_xy_list,
@@ -479,7 +467,7 @@ def parse_args():
 
     parser.add_argument('--test_mode', type=str, default='pos', help='Test with positive pairs')
 
-    parser.add_argument('--query_mode', type=str, default='DImg2SImg', help='Retrieval with drone image to satellite image')
+    parser.add_argument('--query_mode', type=str, default='D2S', help='Retrieval with drone to satellite')
 
     parser.add_argument('--train_with_recon', action='store_true', help='Train with reconstruction')
 

scripts/prepare_dataset/prepare_lidar.py
@@ -2,46 +2,176 @@ import os
 import numpy as np
 import open3d as o3d
 from tqdm import tqdm
+import cv2
+
 
 data_root = '/home/xmuairmud/data/GTA-UAV-data/Lidar/drone/'
 
-# 定义目录路径
-lidar_dir = data_root + "lidars"
-image_dir = data_root + "images"
-meta_dir = data_root + "meta_data"
 
-# 获取文件名
-lidar_files = [f for f in os.listdir(lidar_dir) if f.endswith(".ply")]
+def create_top_down_depth_image(points, bev_height=1080, bev_width=1920):
+    """
+    将点云数据生成俯视角深度图像（鸟瞰图），自动计算投影范围。
 
-for lidar_file in tqdm(lidar_files, desc="Processing lidar files"):
-    lidar_path = os.path.join(lidar_dir, lidar_file)
-    
-    # 读取点云文件
-    pcd = o3d.io.read_point_cloud(lidar_path)
-    points = np.asarray(pcd.points)
-    
-    # 检查点是否为 NaN 或 inf
-    valid_points = points[~np.isnan(points).any(axis=1) & ~np.isinf(points).any(axis=1)]
-    
-    # 如果所有点都无效，删除文件
-    if valid_points.shape[0] == 0:
-        print(f"Deleting {lidar_file} and corresponding files...")
-        
-        # 删除lidars, images, meta_data中的文件
-        os.remove(lidar_path)
-        image_path = os.path.join(image_dir, lidar_file.replace('.ply', '.png'))  # 假设图像为jpg
-        meta_path = os.path.join(meta_dir, lidar_file.replace('.ply', '.txt'))  # 假设meta_data为json
-        if os.path.exists(image_path):
-            os.remove(image_path)
-        if os.path.exists(meta_path):
-            os.remove(meta_path)
-    
+    Args:
+        points: 点云数据，形状为 (N, 3)，每个点包含 (x, y, z)
+        bev_height: 输出图像的高度（像素）
+        bev_width: 输出图像的宽度（像素）
+
+    Returns:
+        depth_map_uint8: 归一化后的深度图像，dtype 为 uint8
+    """
+    # 提取点云坐标
+    x_points = points[:, 0]
+    y_points = points[:, 1]
+    z_points = points[:, 2]
+
+    # 自动计算投影范围
+    x_min = np.min(x_points)
+    x_max = np.max(x_points)
+    y_min = np.min(y_points)
+    y_max = np.max(y_points)
+
+    # 如果需要，可以添加边距
+    padding = 0  # 根据需要调整边距大小
+    x_min -= padding
+    x_max += padding
+    y_min -= padding
+    y_max += padding
+
+    # 计算范围和分辨率
+    x_range = x_max - x_min
+    y_range = y_max - y_min
+    x_resolution = x_range / (bev_width - 1)    # 修改此处
+    y_resolution = y_range / (bev_height - 1)   # 修改此处
+
+    # 将物理坐标转换为图像坐标
+    x_img = ((x_points - x_min) / x_resolution).astype(np.int32)
+    y_img = ((y_points - y_min) / y_resolution).astype(np.int32)
+
+    # 确保索引在合法范围内
+    x_img = np.clip(x_img, 0, bev_width - 1)
+    y_img = np.clip(y_img, 0, bev_height - 1)
+
+    # 将图像坐标系原点设置在左下角
+    y_img = bev_height - y_img - 1
+
+    # 初始化深度图
+    depth_map = np.zeros((bev_height, bev_width), dtype=np.float32)
+
+    # 处理遮挡关系，保留最高点（z 值最大的点）
+    indices = np.lexsort((-z_points, y_img, x_img))
+    x_img = x_img[indices]
+    y_img = y_img[indices]
+    z_points = z_points[indices]
+    coords = np.stack((y_img, x_img), axis=1)
+    _, unique_indices = np.unique(coords, axis=0, return_index=True)
+    depth_map[y_img[unique_indices], x_img[unique_indices]] = z_points[unique_indices]
+    # print(depth_map[0][0])
+    # 将深度值归一化到 [0, 255]
+    non_zero_depth = depth_map[depth_map != 0]
+    if len(non_zero_depth) > 0:
+        max_height = np.percentile(non_zero_depth, 99)
+        min_height = np.percentile(non_zero_depth, 1)
+        depth_map_clipped = np.clip(depth_map, min_height, max_height)
+        depth_map_normalized = (depth_map_clipped - min_height) / (max_height - min_height)
+        depth_map_uint8 = (depth_map_normalized * 255).astype(np.uint8)
     else:
-        # 更新点云文件，去除无效点
-        new_pcd = o3d.geometry.PointCloud()
-        new_pcd.points = o3d.utility.Vector3dVector(valid_points)
-        
-        # 重新保存点云文件
-        o3d.io.write_point_cloud(lidar_path, new_pcd)
+        depth_map_uint8 = depth_map.astype(np.uint8)
+    
+    
+    colored_depth_map = cv2.applyColorMap(depth_map_uint8, cv2.COLORMAP_JET)
 
-print("Processing complete.")
+    return depth_map_uint8, colored_depth_map
+
+
+def data_clean():
+
+    # 定义目录路径
+    lidar_dir = data_root + "lidars"
+    image_dir = data_root + "images"
+    meta_dir = data_root + "meta_data"
+
+    # 获取文件名
+    lidar_files = [f for f in os.listdir(lidar_dir) if f.endswith(".ply")]
+
+    for lidar_file in tqdm(lidar_files, desc="Processing lidar files"):
+        lidar_path = os.path.join(lidar_dir, lidar_file)
+        
+        # 读取点云文件
+        pcd = o3d.io.read_point_cloud(lidar_path)
+        points = np.asarray(pcd.points)
+        
+        # 检查点是否为 NaN 或 inf
+        valid_points = points[~np.isnan(points).any(axis=1) & ~np.isinf(points).any(axis=1)]
+        
+        # 如果所有点都无效，删除文件
+        if valid_points.shape[0] == 0:
+            print(f"Deleting {lidar_file} and corresponding files...")
+            
+            # 删除lidars, images, meta_data中的文件
+            os.remove(lidar_path)
+            image_path = os.path.join(image_dir, lidar_file.replace('.ply', '.png'))  # 假设图像为jpg
+            meta_path = os.path.join(meta_dir, lidar_file.replace('.ply', '.txt'))  # 假设meta_data为json
+            if os.path.exists(image_path):
+                os.remove(image_path)
+            if os.path.exists(meta_path):
+                os.remove(meta_path)
+        
+        else:
+            # 更新点云文件，去除无效点
+            new_pcd = o3d.geometry.PointCloud()
+            new_pcd.points = o3d.utility.Vector3dVector(valid_points)
+            
+            # 重新保存点云文件
+            o3d.io.write_point_cloud(lidar_path, new_pcd)
+
+    print("Processing complete.")
+
+
+def lidar2rgbd():
+    lidar_dir = data_root + "lidars"
+    image_dir = data_root + "images"
+    
+    rgbd_dir = data_root + "rgbd"
+
+    lidar_files = [f for f in os.listdir(lidar_dir) if f.endswith(".ply")]
+
+    for lidar_file in tqdm(lidar_files, desc="Processing lidar files"):
+        lidar_path = os.path.join(lidar_dir, lidar_file)
+
+        pcd = o3d.io.read_point_cloud(lidar_path)
+        points = np.asarray(pcd.points)
+
+        points[:, [0, 2]] = points[:, [2, 0]]  # 交换第0列(x)和第2列(z)
+        points[:, [0, 1]] = points[:, [1, 0]]  # 交换第0列(x)和第2列(z)
+        points[:, 0] = -points[:, 0]
+        points[:, 1] = points[:, 1]
+        points[:, 2] = 10000-points[:, 2]
+
+        depth_img, _ = create_top_down_depth_image(points)
+
+        image_path = os.path.join(image_dir, lidar_file.replace('.ply', '.png'))
+
+        bgr_img = cv2.imread(image_path)
+        rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+
+        if rgb_img.shape[:2] != depth_img.shape[:2]:
+            print(f"ERROR! Wrong size for {image_path}, shape={image_path.shape}")
+            continue
+
+        rgbd_img = np.dstack((rgb_img, depth_img))
+
+        cv2.imwrite(os.path.join(rgbd_dir, lidar_file.replace('.ply', '.png')), rgbd_img)
+    
+    print('Lidar 2 RGBD Done!')
+
+
+if __name__ == "__main__":
+    # lidar2rgbd()
+
+    rgbd = cv2.imread('/home/xmuairmud/data/GTA-UAV-data/Lidar/drone/rgbd/200_0001_0000004776.png', cv2.IMREAD_UNCHANGED)
+    bgr = cv2.imread('/home/xmuairmud/data/GTA-UAV-data/Lidar/drone/images/200_0001_0000004776.png')
+
+    print((rgbd[:, :, 0]==bgr[:, :, 2]).sum(), (rgbd[:, :, 1]==bgr[:, :, 1]).sum(), (rgbd[:, :, 2]==bgr[:, :, 0]).sum())
+    print(rgbd.shape, bgr.shape)
+