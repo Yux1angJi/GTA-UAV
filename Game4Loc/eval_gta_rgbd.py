f230907107246cde68b2da5ab65aca1a0e8d6ada
Yux1angJi <843176830@qq.com>
2024-11-06 04:15:24 -0500

    add eval scripts for rgbd and fix bug for visloc eval

--- Game4Loc/eval_gta_rgbd.py
+++ Game4Loc/eval_gta_rgbd.py
@@ -3,9 +3,9 @@ import torch
 from dataclasses import dataclass
 from torch.utils.data import DataLoader
 
-from game4loc.dataset.visloc import VisLocDatasetEval, get_transforms
-from game4loc.evaluate.visloc import evaluate
-from game4loc.models.model import DesModel
+from game4loc.dataset.gta_rgbd import GTARGBDDatasetEval, get_transforms
+from game4loc.evaluate.gta_rgbd import evaluate
+from game4loc.models.model_rgbd import DesModelWithRGBD
 
 
 @dataclass
@@ -20,33 +20,10 @@ class Configuration:
     
     # Evaluation
     batch_size: int = 128
-    batch_size_eval: int = 128
-
     verbose: bool = True
     gpu_ids: tuple = (0,1)
     normalize_features: bool = True
     eval_gallery_n: int = -1             # -1 for all or int
-
-    test_mode: str = 'pos'
-
-    query_mode = 'D2S'
-
-    # Dataset
-    dataset: str = 'VisLoc-D2S'           # 'U1652-D2S' | 'U1652-S2D'
-
-    # Checkpoint to start from
-    # checkpoint_start = 'work_dir/university/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/162436/weights_end.pth'
-    # checkpoint_start = 'work_dir/sues/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/0723160833/weights_end.pth'
-    # checkpoint_start = 'work_dir/denseuav/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/0723164458/weights_end.pth'
-    # checkpoint_start = 'work_dir/gta/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/0722110449/weights_end.pth'
-    
-    # checkpoint_start = 'work_dir/visloc/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/0723224548/weights_end.pth' ## GTA-UAV
-    # checkpoint_start = 'work_dir/visloc/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/0724003322/weights_end.pth' ## DenseUAV
-    # checkpoint_start = 'work_dir/visloc/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/0724022105/weights_end.pth' ## SUES-200
-    # checkpoint_start = 'work_dir/visloc/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/0723205823/weights_end.pth' ## ImageNet
-    # checkpoint_start = 'work_dir/visloc/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/0724145818/weights_end.pth' ## University
-
-    checkpoint_start = './pretrained/gta/cross_area/game4loc.pth'
 
     # set num_workers to 0 if on Windows
     num_workers: int = 0 if os.name == 'nt' else 4 
@@ -54,10 +31,22 @@ class Configuration:
     # train on GPU if available
     device: str = 'cuda' if torch.cuda.is_available() else 'cpu' 
 
-    data_root = '/root/lanyun-tmp/UAV_VisLoc_dataset_Lidar'
+    # Dataset
+    query_mode: str = 'D2S'
+    # query_mode: str = 'S2D'
+    test_mode: str = 'pos'
 
-    train_pairs_meta_file = 'cross-area-drone2sate-train-z31.json'
-    test_pairs_meta_file = 'cross-area-drone2sate-test-z31.json'
+    # Checkpoint to start from
+    # checkpoint_start = 'pretrained/university/convnext_base.fb_in22k_ft_in1k_384/weights_e1_0.9515.pth'
+    # checkpoint_start = 'work_dir/denseuav/convnext_base.fb_in22k_ft_in1k_384/0630155817/weights_end.pth'
+    # checkpoint_start = 'work_dir/sues/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/0810002619/weights_end.pth'
+    # checkpoint_start = 'work_dir/sues/vit_base_patch16_rope_reg1_gap_256.sbb_in1k/0809045532/weights_end.pth'
+    checkpoint_start = '/root/lanyun-tmp/jyx/GTA-UAV/Game4Loc/pretrained/gta/cross_area/licogeo.pth'
+
+    data_root: str = "/root/lanyun-tmp/GTA-UAV-Lidar-LR"
+
+    train_pairs_meta_file = 'cross-area-drone2sate-train-12.json'
+    test_pairs_meta_file = 'cross-area-drone2sate-test-12.json'
     sate_img_dir = 'satellite'
 
 
@@ -65,8 +54,7 @@ class Configuration:
 # Config                                                                      #
 #-----------------------------------------------------------------------------#
 
-config = Configuration() 
-
+config = Configuration()
 
 
 if __name__ == '__main__':
@@ -77,8 +65,7 @@ if __name__ == '__main__':
     
     print("\nModel: {}".format(config.model))
 
-
-    model = DesModel(config.model,
+    model = DesModelWithRGBD(config.model,
                           pretrained=True,
                           img_size=config.img_size)
                           
@@ -114,35 +101,42 @@ if __name__ == '__main__':
     #-----------------------------------------------------------------------------#
 
     # Transforms
-    val_transforms, train_sat_transforms, train_drone_transforms = get_transforms(img_size, mean=mean, std=std)
+    val_sat_transforms, val_drone_rgb_transforms, val_drone_depth_transforms, train_sat_transforms, \
+        train_drone_geo_transforms, train_drone_rgb_transforms, train_drone_depth_transforms \
+         = get_transforms(img_size, mean=mean, std=std)
 
 
     # Test query
-    query_dataset_test = VisLocDatasetEval(data_root=config.data_root,
+    query_dataset_test = GTARGBDDatasetEval(data_root=config.data_root,
                                         pairs_meta_file=config.test_pairs_meta_file,
-                                        view="drone",
+                                        view=query_view,
+                                        transforms_rgb=val_drone_rgb_transforms,
+                                        transforms_depth=val_drone_depth_transforms,
                                         mode=config.test_mode,
-                                        transforms=val_transforms,
+                                        sate_img_dir=config.sate_img_dir,
+                                        query_mode=config.query_mode,
                                         )
     query_img_list = query_dataset_test.images_name
-    pairs_drone2sate_dict = query_dataset_test.pairs_drone2sate_dict
     query_loc_xy_list = query_dataset_test.images_loc_xy
+    pairs_drone2sate_dict = query_dataset_test.pairs_drone2sate_dict
     
     query_dataloader_test = DataLoader(query_dataset_test,
                                        batch_size=config.batch_size_eval,
                                        num_workers=config.num_workers,
                                        shuffle=False,
                                        pin_memory=True)
-
+    
     # Test gallery
-    gallery_dataset_test = VisLocDatasetEval(data_root=config.data_root,
-                                               pairs_meta_file=config.test_pairs_meta_file,
-                                               view="sate",
-                                               transforms=val_transforms,
-                                               sate_img_dir=config.sate_img_dir,
-                                               )
-    gallery_img_list = gallery_dataset_test.images_name
+    gallery_dataset_test = GTARGBDDatasetEval(data_root=config.data_root,
+                                          pairs_meta_file=config.test_pairs_meta_file,
+                                          view=gallery_view,
+                                          transforms_rgb=val_sat_transforms,
+                                          mode=config.test_mode,
+                                          sate_img_dir=config.sate_img_dir,
+                                          query_mode=config.query_mode,
+                                         )
     gallery_loc_xy_list = gallery_dataset_test.images_loc_xy
+    gallery_img_list = gallery_dataset_test.images_name
     
     gallery_dataloader_test = DataLoader(gallery_dataset_test,
                                        batch_size=config.batch_size_eval,
@@ -153,7 +147,7 @@ if __name__ == '__main__':
     print("Query Images Test:", len(query_dataset_test))
     print("Gallery Images Test:", len(gallery_dataset_test))
     
-    print("\n{}[{}]{}".format(30*"-", "UAV-VisLoc", 30*"-"))  
+    print("\n{}[{}]{}".format(30*"-", "GTA-VisLoc", 30*"-"))  
 
     r1_test = evaluate(config=config,
                            model=model,
@@ -161,10 +155,13 @@ if __name__ == '__main__':
                            gallery_loader=gallery_dataloader_test, 
                            query_list=query_img_list,
                            gallery_list=gallery_img_list,
-                           query_loc_xy_list=query_loc_xy_list,
-                           gallery_loc_xy_list=gallery_loc_xy_list,
                            pairs_dict=pairs_drone2sate_dict,
                            ranks_list=[1, 5, 10],
+                           query_loc_xy_list=query_loc_xy_list,
+                           gallery_loc_xy_list=gallery_loc_xy_list,
                            step_size=1000,
-                           cleanup=True)
- 
+                           cleanup=True,
+                           plot_acc_threshold=True,
+                           top10_log=False)
+
+
